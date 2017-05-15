ec4e5e94c1a79d545caeff3329555d7cb13c4efc
Wang Ru <rockidog@live.com>
2017-05-15 10:59:46 +0800

    Add an abstract probability distribution class

--- cpp/random.h
+++ cpp/random.h
@@ -5,12 +5,36 @@
 #include <random>
 
 /*
+ * Generic random distribution class for float.
+ *
+ * Generates a random variable subjected to a specific distribution.
+ * Also provides a function to compute the PDF for a given value.
+ */
+class Distribution {
+ public:
+  /*
+   * When no parameter is given, generates the next random value.
+   *
+   * All derived classes should override this function.
+   */
+  virtual float operator()() = 0;
+
+
+  /*
+   * Compute the PDF for a given value.
+   *
+   * All derived classes should override this function.
+   */
+  virtual float operator()(float y) = 0;
+};
+
+/*
  * Consine weighted distribution for float.
  *
  * Generates a consine weighted distributed variable from [0, π/2].
  * Also provides a function to compute the PDF for a given value.
  */
-class Cosine {
+class Cosine : public Distribution {
  public:
   Cosine(int seed = 0) : generator_(seed), uniform_(0, 1) {}
 
@@ -21,7 +45,7 @@ class Cosine {
    * where p(x)=1, then let Y=arcsin(X) is subjected to
    * the new distribution Y~P(Y), where p(y) = cos(y).
    */
-  float operator()() { return asin(uniform_(generator_)); }
+  virtual float operator()() override { return asin(uniform_(generator_)); }
 
   /*
    * Compute the PDF for a given value.
@@ -29,7 +53,7 @@ class Cosine {
    * The input should be in the range of [0, π/2].
    * Otherwise the ouput will be zero.
    */
-  float operator()(float y) { return cos(y); }
+  virtual float operator()(float y) override { return cos(y); }
 
  private:
   std::mt19937 generator_;
