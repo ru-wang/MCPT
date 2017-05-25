1cceae400755a672bdd16b7720a572df25aa2238
Wang Ru <rockidog@live.com>
2017-05-25 23:13:31 +0800

    Rename ray_cast.h and extend the RayCaster class

---
+++ cpp/ray_caster.cc
@@ -0,0 +1,95 @@
+#include "ray_caster.h"
+
+#include "eigen.h"
+#include "geometry.h"
+#include "intersection.h"
+#include "object.h"
+#include "scene.h"
+
+#include <limits>
+#include <queue>
+#include <tuple>
+
+using namespace std;
+
+tuple<Point, Vector3f, string>
+RayCaster::operator()(const Ray& r) const {
+  Intersection intersectant;
+
+  Point intersection = Point::Zero();
+  float min_t = -1;
+
+  const Mesh* mesh = nullptr;
+  string mtl_name;
+
+  /* computes intersections with all the objects, selects the closest one */
+  for (const auto& entry : scene_->objects()) {
+    const Object& obj = entry.second;
+    tuple<Point, float, size_t> tp = cast(r, obj, intersectant);
+    float t = get<1>(tp);
+    if (!Intersection::IsZero(get<0>(tp).w()) && (t < min_t || min_t < 0)) {
+      intersection = get<0>(tp);
+      min_t = t;
+
+      mesh = &obj.GetMeshByMeshID(get<2>(tp));
+      mtl_name = obj.GetMaterialNameByMeshID(get<2>(tp));
+    }
+  }
+
+  if (min_t > 0) {
+    /* computes the normal vector */
+    Vector3f normal = Vector3f::Zero();
+    if (mesh->v_num() == 3) {
+      const TriMesh* tri = dynamic_cast<const TriMesh*>(mesh);
+      for (size_t i = 0; i < mesh->v_num(); ++i)
+        normal += scene_->vn()[tri->vn_id[0]];
+      normal /= mesh->v_num();
+    } else if (mesh->v_num() == 4) {
+      const RectMesh* rect = dynamic_cast<const RectMesh*>(mesh);
+      for (size_t i = 0; i < mesh->v_num(); ++i)
+        normal += scene_->vn()[rect->vn_id[0]];
+      normal /= mesh->v_num();
+    }
+    normal.NormalizeInPlace();
+    return make_tuple(intersection, normal, mtl_name);
+  } else {
+    Vector3f nan = Vector3f::All(numeric_limits<float>::quiet_NaN());
+    return make_tuple(intersection, nan, mtl_name);
+  }
+}
+
+tuple<Point, float, size_t>
+RayCaster::cast(const Ray& r, const Object& obj, const Intersection& intersectant) const {
+  Point intersection = Point::Zero();
+  size_t mesh_id = 0;
+  float min_t = -1;
+
+  /* traverses the BVH tree using BFS */
+  queue<const Object::BVH*> queue;
+  queue.push(obj.bvh_root());
+  while (!queue.empty()) {
+    const Object::BVH* current_bvh = queue.front();
+    const AABB& aabb = current_bvh->aabb;
+    queue.pop();
+
+    if (!intersectant(r, aabb))
+      continue;
+
+    if (current_bvh->nonleaf()) {
+      queue.push(current_bvh->l_child);
+      if (current_bvh->r_child)
+        queue.push(current_bvh->r_child);
+    } else if (current_bvh->leaf()) {
+      Polygon f = obj.GetPolygonByMeshID(current_bvh->mesh_id);
+      Point x = intersectant(r, f);
+      float t = (x - r.A) * Vector4f(r.dir);
+      if (!Intersection::IsZero(x.w()) && (t < min_t || min_t < 0)) {
+        intersection = x;
+        mesh_id = current_bvh->mesh_id;
+        min_t = t;
+      }
+    }
+  }
+
+  return make_tuple(intersection, min_t, mesh_id);
+}
---
+++ cpp/ray_caster.h
@@ -0,0 +1,35 @@
+#ifndef MCPT_RAY_CASTER_H_
+#define MCPT_RAY_CASTER_H_
+
+#include "eigen.h"
+#include "geometry.h"
+
+#include <tuple>
+
+class Intersection;
+class Object;
+class Scene;
+
+class RayCaster {
+ public:
+  RayCaster(const Scene* const scene) : scene_(scene) {}
+
+  /*
+   * Returns the intersection and the normal at the intersection of the ray
+   * and the objects in the scene, as well as the material name of the mesh.
+   */
+  std::tuple<Point, Vector3f, std::string> operator()(const Ray& r) const;
+
+ private:
+
+  /*
+   * Returns the intersection of the ray and an object, and its projection length
+   * on the ray, as well as the material name of the mesh.
+   */
+  std::tuple<Point, float, size_t>
+  cast(const Ray& r, const Object& obj, const Intersection& intersectant) const;
+
+  const Scene* const scene_;
+};
+
+#endif  /* MCPT_RAY_CASTER_H_ */
