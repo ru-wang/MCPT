bee7548c3b089c55f4c221896175c3577aae3aac
Wang Ru <rockidog@live.com>
2017-05-25 23:16:07 +0800

    Add a path tracing class

---
+++ cpp/path_tracer.cc
@@ -0,0 +1,87 @@
+#include "path_tracer.h"
+
+#include "geometry.h"
+#include "scene.h"
+
+#include <cmath>
+#include <string>
+#include <tuple>
+
+using namespace std;
+
+PathTracer::Path PathTracer::operator()(const Ray& r, float Ni) {
+  Point intersection;
+  Vector3f normal;
+  string mtl_name;
+  tie(intersection, normal, mtl_name) = caster_(r);
+  const Material& mtl = scene_->materials().find(mtl_name)->second;
+
+  /* No intersection, returns an invalid path with 0 probability. */
+  if (isnan(normal.x()) || isnan(normal.y()) || isnan(normal.z()))
+    return PathTracer::Path(r, normal, 0, mtl);
+
+  const Vector3f& L = r.dir;
+  const Vector3f& N = normal;
+  float cos_omega = -(L * N);
+
+  /* opaque */
+  if (mtl.Tr == 0) {
+    if (cos_omega > 0) {
+      Vector3f Nz = N;
+      Vector3f Nx = (L + N * cos_omega).Normalize();
+      Vector3f Ny = Cross(Nz, Nx).Normalize();
+
+      Vector3f V;
+      float phi, theta, p;
+      do {
+        tie(phi, theta, p) = random_();
+        float sin_theta = sin(theta);
+        Vector3f H = cos(phi) * sin_theta * Nx +
+          sin(phi) * sin_theta * Ny +
+          cos(theta) * Nz;
+        H.NormalizeInPlace();
+        V = L - 2 * (L * H) * H;
+      } while (V * N <= 0);
+
+      return PathTracer::Path(Ray(intersection, V), N, p, mtl);
+    } else {
+      /* Intersects at a back surface, returns an invalid path with 0 probability. */
+      return PathTracer::Path(r, normal, 0, mtl);
+    }
+  }
+
+  /* transparent */
+  else {
+    if (cos_omega > 0) {
+      /* enters a transparent object */
+      float sin_omega = sqrt(1 - cos_omega * cos_omega);
+      float sin_theta = sin_omega * Ni / mtl.Ni;
+      float cos_theta = sqrt(1 - sin_theta * sin_theta);
+
+      Vector3f Npar = N;
+      Vector3f Nper = (L + N * cos_omega).Normalize();
+      Vector3f V = Nper * sin_theta - Npar * cos_theta;
+      float p = 1;
+
+      return PathTracer::Path(Ray(intersection, V), N, p, mtl);
+    } else {
+      /* leaves a transparent object */
+      Vector3f Nz = N;
+      Vector3f Nx = (L + N * cos_omega).Normalize();
+      Vector3f Ny = Cross(Nz, Nx).Normalize();
+
+      Vector3f V;
+      float phi, theta, p;
+
+      do {
+        tie(phi, theta, p) = random_();
+        float sin_theta = sin(theta);
+        V = cos(phi) * sin_theta * Nx +
+            sin(phi) * sin_theta * Ny +
+            cos(theta) * Nz;
+      } while (V * N <= 0);
+
+      return PathTracer::Path(Ray(intersection, V), N, p, mtl);
+    }
+  }
+}
--- cpp/path_tracer.h
+++ cpp/path_tracer.h
@@ -1,7 +1,42 @@
 #ifndef MCPT_PATH_TRACER_H_
 #define MCPT_PATH_TRACER_H_
 
+#include "eigen.h"
+#include "geometry.h"
+#include "random.h"
+#include "ray_caster.h"
+
+class Scene;
+struct Material;
+
 class PathTracer {
+ public:
+  struct Path {
+    Path(const Ray& r, const Vector3f& N, float p, const Material& mtl)
+        : r(r), N(N), p(p), mtl(mtl) {}
+
+    Ray r;       /* the ray of the path */
+    Vector3f N;  /* the normal at the interface */
+    float p;     /* the probability */
+    const Material& mtl;
+  };
+
+  PathTracer(const Scene* const scene) : caster_(scene), scene_(scene) {}
+
+  /*
+   * Returns the new output path at the intersection of the input ray
+   * and the surface in the scene, given the geometry relationship
+   * as well as the material information.
+   *
+   * The optional Ni specifies the index of refraction of the space where
+   * the light is traveling. The default value is 1, meaning the vacuo.
+   */
+  Path operator()(const Ray& r, float Ni = 1);
+
+ private:
+  RayCaster caster_;
+  CosineWeightedHemisphere random_;
+  const Scene* const scene_;
 };
 
 #endif  /* MCPT_PATH_TRACER_H_ */
