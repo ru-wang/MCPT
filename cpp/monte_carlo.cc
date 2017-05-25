f06e563afe4a984d7cae3fc6c6cb3b8683463a8c
Wang Ru <rockidog@live.com>
2017-05-25 23:16:33 +0800

    Add a Monte Carlo integrator class

---
+++ cpp/monte_carlo.cc
@@ -0,0 +1,111 @@
+#include "monte_carlo.h"
+
+#include "eigen.h"
+#include "geometry.h"
+#include "object.h"
+#include "path_tracer.h"
+#include "random.h"
+
+#include <cassert>
+#include <cmath>
+
+using namespace std;
+
+void MonteCarlo::operator()() {
+  assert(are_params_set_ && "Should set up parameters frist!");
+
+  result_ = new float[w_ * h_ * 3]{0};
+  for (int n = 0; n < n_max_; ++n) {
+    for (int k = 0; k < k_max_; ++k) {
+      for (int u = 0; u < w_; ++u) {
+        for (int v = 0; v < h_; ++v) {
+          for (int i = 0; i < rays_per_pixel_; ++i) {
+            Vector3f color = sample(u, v, k);
+            int p = (w_ * v + u) * 3;
+            result_[p + 0] += color.r();
+            result_[p + 1] += color.g();
+            result_[p + 2] += color.b();
+          }
+        }
+      }
+    }
+  }
+  for (int p = 0; p < w_ * h_ * 3; p += 3)
+    result_[p] /= n_max_;
+}
+
+Vector3f MonteCarlo::sample(int u, int v, int k) {
+  Point view_point = MVP_inv_ * Vector4f(u, v, 1, 1);
+  Vector3f view_dir = Vector3f(MVP_inv_ * Vector4f(u, v, 1, 0));
+
+  vector<PathTracer::Path> paths;
+  Ray r(view_point, view_dir);
+  float Ni = 1;
+  for (int i = 0; i < k; ++i) {
+    PathTracer::Path path = tracer_(r, Ni);
+    if (path.p == 0)
+      break;
+    else
+      paths.push_back(path);
+    r = path.r;
+    Ni = path.mtl.Ni;
+  }
+
+  /* Prepares for the first light transport. */
+  float probability = 1;
+  Vector3f Ii;
+  Vector3f Ia, Id, Is;
+  Vector3f V, L, N, H;
+
+  for (auto rit = paths.crbegin(); rit != paths.crend(); ++rit) {
+    const Material& mtl = rit->mtl;
+
+    /* opaque: BRDF of Blinn-Phong model */
+    if (mtl.Tr == 0) {
+      V = -rit->r.dir;
+      N = rit->N;
+      H = (L + V).Normalize();
+
+      Ia = mtl.Ka;
+      Id.SetZero();
+      Is.SetZero();
+      float cos_phi = N * L;
+      float cos_theta = H * N;
+      if (cos_phi > 0)
+        Id = Vector3f(Ii.x() * mtl.Kd.x(),
+                      Ii.y() * mtl.Kd.y(),
+                      Ii.z() * mtl.Kd.z()) * cos_phi;
+      if (cos_theta > 0)
+        Is = Vector3f(Ii.x() * mtl.Ks.x(),
+                      Ii.y() * mtl.Ks.y(),
+                      Ii.z() * mtl.Ks.z()) * pow(cos_theta, mtl.Ns);
+      Ii = Ia + Id + Is;
+
+      L = V;
+      probability *= rit->p;
+    }
+
+    /* transparent: BTDF of Lambertian model */
+    else {
+      V = -rit->r.dir;
+      N = rit->N;
+
+      Ia = mtl.Ka;
+      Id.SetZero();
+      float cos_phi = N * (-L);
+      if (cos_phi > 0)
+        Id = mtl.Tr * Vector3f(Ii.x() * mtl.Kd.x(),
+                               Ii.y() * mtl.Kd.y(),
+                               Ii.z() * mtl.Kd.z()) * cos_phi;
+      else
+        Id = mtl.Tr * Ii;
+      Ii = Ia + Id;
+
+      L = V;
+      probability *= rit->p;
+    }
+  }
+
+  return Ii;
+}
+
--- cpp/monte_carlo.h
+++ cpp/monte_carlo.h
@@ -1,4 +1,62 @@
-#ifndef MCPT_PATH_TRACER_H_
-#define MCPT_PATH_TRACER_H_
+#ifndef MCPT_MONTE_CARLO_H_
+#define MCPT_MONTE_CARLO_H_
 
-#endif  /* MCPT_PATH_TRACER_H_ */
+#include "eigen.h"
+#include "path_tracer.h"
+
+class Scene;
+
+  /*
+   * Monte Carlo integrator class.
+   *
+   * Use BRDF(Bidirectional reflection distribution function) of Blinn-Phong model
+   * and BTDF(Bidirectional transmittance distribution function) of Lambert model.
+   */
+class MonteCarlo {
+ public:
+  MonteCarlo(const Scene* const scene)
+      : are_params_set_(false), result_(nullptr), tracer_(scene), scene_(scene) {}
+
+  ~MonteCarlo() { if (result_) delete result_; }
+
+  const float* result() const { return result_; }
+
+  void SetParameters(int width, int height,
+                     const Matrix4f& MVP_inverse,
+                     int k_max, int n_max, int rays_per_pixel) {
+    w_ = width; h_ = height;
+    MVP_inv_ = MVP_inverse;
+    k_max_ = k_max; n_max_ = n_max;
+    rays_per_pixel_ = rays_per_pixel;
+    are_params_set_ = true;
+  }
+
+  void operator()();
+
+ private:
+  /*
+   * Samples a path with k points and returns the color at the pixel (u, v).
+   */
+  Vector3f sample(int u, int v, int k);
+
+  /*
+   * Parameters for the virtual camera.
+   */
+  int w_, h_;
+  Matrix4f MVP_inv_;
+
+  /*
+   * Parameters used by the Monte Carlo integrator.
+   */
+  int k_max_;           /* maximum path length for path tracer */
+  int n_max_;           /* maximum sampling times for Monte Carlo integrator */
+  int rays_per_pixel_;  /* eye rays per pixel */
+
+  bool are_params_set_;
+  float* result_;
+
+  PathTracer tracer_;
+  const Scene* const scene_;
+};
+
+#endif  /* MCPT_MONTE_CARLO_H_ */
