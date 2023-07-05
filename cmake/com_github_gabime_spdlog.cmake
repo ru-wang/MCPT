441c3105d48595e51c0086f8a81eeb5cc74dbcfa
Wang Ru <rockidog@live.com>
2023-07-06 02:00:17 +0800

    Fix spdlog version

--- cmake/com_github_gabime_spdlog.cmake
+++ cmake/com_github_gabime_spdlog.cmake
@@ -1,6 +1,6 @@
 FetchContent_Declare(com.github.gabime.spdlog
   GIT_REPOSITORY https://github.com/gabime/spdlog.git
-  GIT_TAG        v1.x
+  GIT_TAG        v1.11.0
   GIT_SHALLOW    TRUE)
 FetchContent_MakeAvailable(com.github.gabime.spdlog)
 
