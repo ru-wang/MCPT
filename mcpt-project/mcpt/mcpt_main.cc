738ecdf9445730d031960ca244b72098316bf610
Wang Ru <rockidog@live.com>
2023-07-20 20:32:02 +0800

    Change output file path

--- mcpt-project/mcpt/mcpt_main.cc
+++ mcpt-project/mcpt/mcpt_main.cc
@@ -1,3 +1,4 @@
+#include <ctime>
 #include <filesystem>
 #include <fstream>
 #include <memory>
@@ -8,6 +9,7 @@
 #include <Eigen/Eigen>
 #include <cheers/window/window.hpp>
 #include <spdlog/fmt/fmt.h>
+#include <spdlog/fmt/chrono.h>
 #include <spdlog/fmt/ostr.h>
 #include <spdlog/spdlog.h>
 #include <spdlog/stopwatch.h>
@@ -36,7 +38,7 @@ Object LoadObject(std::filesystem::path obj_path) {
 int main(int argc, char* argv[]) {
   MakeLogger("default");
 
-  ASSERT(argc == 5);
+  ASSERT(argc == 6);
   std::filesystem::path arg_obj_fname = argv[1];
   ASSERT(arg_obj_fname.has_parent_path());
   ASSERT(arg_obj_fname.has_filename());
@@ -45,8 +47,9 @@ int main(int argc, char* argv[]) {
   unsigned int arg_h = std::stoul(argv[3]);
   ASSERT(arg_w > 0 && arg_h > 0);
 
-  size_t arg_samples = std::stoul(argv[4]);
-  ASSERT(arg_samples > 0);
+  size_t arg_spp = std::stoul(argv[4]);
+  size_t arg_spp_save_interval = std::stoul(argv[5]);
+  ASSERT(0 < arg_spp_save_interval && arg_spp_save_interval <= arg_spp);
 
   spdlog::info("loading object from {}", arg_obj_fname);
   SandboxFileserver fserver(arg_obj_fname.parent_path());
@@ -73,17 +76,22 @@ int main(int argc, char* argv[]) {
   auto viz_thread = std::thread{VizThread{mc_opts.t * 2.0}};
   object_layer->UpdateObject(obj, mc_opts.R, mc_opts.t);
 
-  size_t samples = 0;
+  size_t spp = 0;
   std::vector integral(arg_w * arg_h * 3, 0.0F);
-  SandboxFileserver fs_out(fmt::format("{}_{}x{}", arg_obj_fname.stem().string(), arg_w, arg_h));
 
-  spdlog::info("running MCPT for {} samples", arg_samples);
+  auto t = fmt::localtime(std::time(nullptr));
+  auto export_dir =
+      fmt::format("out/{:%Y%m%d_%H%M%S}_{}_{}x{}", t, arg_obj_fname.stem().string(), arg_w, arg_h);
+  SandboxFileserver fs_out(export_dir);
+  spdlog::info("results will be saved in {}", fs_out.GetAbsolutePath());
+
+  spdlog::info("running MCPT for spp: {}", arg_spp);
   spdlog::stopwatch sw;
 
 #ifdef NDEBUG
 #pragma omp parallel for
 #endif
-  for (size_t k = 0; k < arg_samples; ++k) {
+  for (size_t k = 0; k < arg_spp; ++k) {
     spdlog::stopwatch sw_k;
 
     std::vector<Eigen::Vector3f> radiance(arg_w * arg_h);
@@ -112,16 +120,17 @@ int main(int argc, char* argv[]) {
         integral[i * 3 + 2] += radiance[i].z();
       }
 
-      spdlog::info("sample: {}/{}, elapsed(s): {:.1f}/{:.1f}", ++samples, arg_samples, elapsed, sw);
-      if (samples == arg_samples || samples % 50 == 0) {
-        spdlog::info("saving to PPM image sample_{}.ppm", samples);
+      spdlog::info("spp: {}/{}, elapsed(s): {:.1f}/{:.1f}", ++spp, arg_spp, elapsed, sw);
+      if (spp == arg_spp || spp % arg_spp_save_interval == 0) {
+        auto export_fname = fmt::format("spp_{}.ppm", spp);
+        spdlog::info("saving to PPM image {}/{}", export_dir, export_fname);
 
         std::vector<float> im(integral.size());
         for (size_t i = 0; i < integral.size(); ++i)
-          im[i] = integral[i] / samples;
+          im[i] = integral[i] / spp;
 
         std::ofstream ofs;
-        ASSERT(fs_out.OpenTextWrite(fmt::format("sample_{}.ppm", samples), ofs));
+        ASSERT(fs_out.OpenTextWrite(export_fname, ofs));
         RawToPPM(arg_w, arg_h, 2.2F, im.data(), ofs);
       }
     }
