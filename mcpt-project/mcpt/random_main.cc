09e1698f9d51157b61783de9ab899a1d6db69131
Wang Ru <rockidog@live.com>
2023-07-25 20:27:39 +0800

    Add visualization test for triangle sampling

--- mcpt-project/mcpt/random_main.cc
+++ mcpt-project/mcpt/random_main.cc
@@ -41,34 +41,51 @@ std::vector<Line<float>> SampleLines(const Eigen::Vector3f& center,
   return lines;
 }
 
+template <typename RndGen>
+std::vector<Eigen::Vector3f> SampleTriangle(const Eigen::Vector3f& a,
+                                            const Eigen::Vector3f& b,
+                                            const Eigen::Vector3f& c,
+                                            RndGen& gen,
+                                            size_t num) {
+  std::vector<Eigen::Vector3f> points;
+  while (num--) {
+    auto arr = gen.Random(a.data(), b.data(), c.data());
+    points.emplace_back(arr.data());
+  }
+  return points;
+}
+
 int main(int argc, char* argv[]) {
   MakeLogger("random");
 
   ASSERT(argc == 3);
   float arg_cos_pow = std::stof(argv[1]);
-  size_t arg_max_samples = std::stoul(argv[2]);
+  size_t arg_samples = std::stoul(argv[2]);
 
   srand(std::random_device{}());
-  Eigen::Vector3f a = Eigen::Vector3f::Random() * 10.0F;
-  Eigen::Vector3f b = Eigen::Vector3f::Random() * 10.0F;
-  Eigen::Vector3f c = Eigen::Vector3f::Random() * 10.0F;
+  Eigen::Vector3f a = (Eigen::Vector2f::Random() * 10.0F).homogeneous();
+  Eigen::Vector3f b = (Eigen::Vector2f::Random() * 10.0F).homogeneous();
+  Eigen::Vector3f c = (Eigen::Vector2f::Random() * 10.0F).homogeneous();
 
-  Eigen::Vector3f normal = (b - a).cross(c - b).normalized();
   Eigen::Vector3f center = (a + b + c) / 3.0F;
+  Eigen::Vector3f normal = (b - a).cross(c - b).normalized();
+  normal /= normal.z();
 
-  std::vector<Line<float>> lines;
-  CosPowHemisphere<float> gen;
-  lines = SampleLines(center, normal, gen, arg_cos_pow, arg_max_samples);
-
+  CosPowHemisphere<float> cos_pow_hemi;
+  auto lines = SampleLines(center, normal, cos_pow_hemi, arg_cos_pow, arg_samples);
   lines.emplace_back(a, b);
   lines.emplace_back(b, c);
   lines.emplace_back(c, a);
 
-  cheers::Window::Instance().InstallSharedLayer<ShapeLayer>()->AddLines(lines);
+  UniformTriangle<float> uni_tri;
+  auto points = SampleTriangle(a, b, c, uni_tri, arg_samples);
+
+  auto layer = cheers::Window::Instance().InstallSharedLayer<ShapeLayer>();
+  layer->AddLines(lines);
+  layer->AddPoints(points);
   cheers::Window::Instance().CreateContext();
   while (cheers::Window::Instance().WaitForWindowExiting())
     ;
   cheers::Window::Instance().DestroyContext();
-
   return 0;
 }
